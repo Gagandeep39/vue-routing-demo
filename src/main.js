6875b656bf94b3cbe4cd34f08de0fe0df1d1821d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-12-16 07:07:32 +0000

    Replaced component with router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue';
-
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-createApp(App).mount('#app');
+import TeamsList from './components/teams/TeamsList';
+import UsersList from './components/users/UsersList';
+
+const router = createRouter({
+  // createWebHistory() Use browsers buildt in history logic
+  history: createWebHistory(), //Ensures user goes to previous page on back press
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
+});
+createApp(App)
+  .use(router)
+  .mount('#app');
